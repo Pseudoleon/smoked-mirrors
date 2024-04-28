21aca16784e6fcb002cb8af0ec571042e21924b8
k4lizen <124312252+k4lizen@users.noreply.github.com>
2024-04-28 10:10:50 +0200

    line coloring fix

--- static/scripts/high.js
+++ static/scripts/high.js
@@ -1,15 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+let correctLineMap = {};
+
+document.addEventListener('DOMContentLoaded', async function() {
     var codeLines = document.querySelectorAll('[id^="codeblockline"]');
-    let counter = 0; // Start from 1 because SQL IDs start from 1
+    let counter = -1; // Start from 1 because SQL IDs start from 1
 
     let targetLine = -1;
-    codeLines.forEach(function(line) {
+    for (let line of codeLines) {
         let lineId = line.getAttribute('id');
+        // console.log("LINE: " + lineId)
         if (lineId == "codeblockline-1") {
+            counter++;
             targetLine = -1;
             let addr = 'http://127.0.0.1:5000/exe/api/' + counter;
-            console.log(addr)
-            fetch(addr, {
+            console.log(addr);
+            await fetch(addr, {
                 method: 'GET',
                 mode: "no-cors"
             })
@@ -18,24 +22,35 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Handle response from server
                 targetLine = data["message"]["line"];
                 console.log("Target line: " + targetLine);
+
+                // console.log("setting " + counter + " to " + targetLine);
+                correctLineMap[counter] = targetLine;
+                // console.log(correctLineMap);
             })
             .catch(error => {
                 console.error('Error:', error);
             });
-            counter++;
         }
 
-        line.className += " hover-overlay"
-        
+        line.className += " hover-overlay";
+        line.setAttribute("messageID", counter);
+
+        // console.log(correctLineMap);
+
         line.addEventListener('click', function() {
+            let messageID = this.getAttribute("messageID");
+            let targ = correctLineMap[messageID];
+            let id = this.getAttribute("id");
+            
+            console.log("click detected for " + id + " of " + messageID + " . target it: " + targ);
+            // console.log(correctLineMap);
+
             let style = "bg-danger";
-            if (lineId == ("codeblockline-" + targetLine)) {
+            if (id == ("codeblockline-" + targ)) {
                 style = "bg-success";
-            } else {
-                console.log(lineId)
             }
             line.className += " " + style;
         });
         
-    });
+    }
 });
